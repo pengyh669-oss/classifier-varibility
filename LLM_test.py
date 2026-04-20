20a881a7a8735d38e9a050f2610bb47d4475eabe
pengyh669-oss <pengyh669@gmail.com>
2026-04-20 11:42:39 +0800

    模型回答-5个

--- LLM_test.py
+++ LLM_test.py
@@ -6,7 +6,7 @@ from llm_experiment_core import build_common_arg_parser, run_experiment
 
 
 FORMAL_COUNT = 360
-FORMAL_OUTPUT_FILE = "LLM_qwen3.6-plus.txt"
+FORMAL_OUTPUT_FILE = "LLM_claude-sonnet-4-6.txt"
 
 
 def main() -> None:
